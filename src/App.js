83cbd035867f78e23c0213334f0f138e99f05b12
zahoorshinwari <khanzshinwari5371@gmail.com>
2024-04-17 23:43:19 +0500

    stripe api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// Frontend: src/components/Checkout.js
 
-function App() {
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import img1 from "./assets/1.jpeg"
+
+
+const App = () => {
+  const handleToken = async (token) => {
+    
+    try {
+      const response = await fetch('http://localhost:5000/api/charge', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token }),
+        
+      });
+
+      console.log("token is " ,token);
+      if (response.ok) {
+        console.log('Payment successful!');
+      } else {
+        console.error('Payment failed!');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StripeCheckout
+    name='checkout'
+    description='this is the checkout method'
+    token={handleToken}
+    
+    image={img1}
+    billingAddress={true}
+    zipCode={true}
+      stripeKey="pk_test_51OuJ16KPPHu93816MAXEBoPiw9vOroVoxgVzLWPA3QmXNbwDKKXu8bekFxVMN6zmfFhSVljeRCddyYjouRG7wwgj00Z1Ebw9Oh"
+      amount={1000} // Amount in cents
+      currency="USD"
+      allowRememberMe={true} // Allow Stripe to save card for future transactions
+    />
   );
-}
+};
 
-export default App;
+export default App;
+
+
+
+
+
